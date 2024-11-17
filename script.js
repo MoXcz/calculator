0cd54bde0c6afe5c88963e42f831f9690478e0af
debian-mocos <130593875+MoXcz@users.noreply.github.com>
2024-11-16 20:21:51 -0500

    Abstract operations
    Operator functionality will be managed by operate(), which will
    facilitate and simplify the action, and now all further need for these
    operators can be under the same abstraction.

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function multipy(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+let firstNumber = 0,
+  operator,
+  secondNumber;
+
+function operate(operator, firstNumber, secondNumber) {
+  switch (operator) {
+    case "+":
+      return add(firstNumber, secondNumber);
+    case "-":
+      return subtract(firstNumber, secondNumber);
+    case "*":
+      return multipy(firstNumber, secondNumber);
+    case "/":
+      return divide(firstNumber, secondNumber);
+  }
+}
